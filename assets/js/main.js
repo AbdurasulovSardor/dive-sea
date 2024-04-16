446bfa0425ff36861946724a608e312f76d7ee02
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-04-16 10:05:34 +0500

    added home section scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,31 @@ window.addEventListener("click", (e) => {
     navList.classList.remove("show-menu")
   }
 })
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
+const homeBtn = document.querySelectorAll(".home__arrow-btn")
+const homeList = document.querySelector(".home__images-list")
+
+let homeActive = false
+
+homeBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    homeActive = !homeActive
+
+    if (homeActive) {
+      homeList.classList.add("home__images-list--active")
+      homeBtn[0].disabled = false
+      homeBtn[0].style.opacity = "1"
+      homeBtn[1].disabled = true
+      homeBtn[1].style.opacity = "0.5"
+    } else {
+      homeList.classList.remove("home__images-list--active")
+      homeBtn[0].disabled = true
+      homeBtn[0].style.opacity = "0.5"
+      homeBtn[1].disabled = false
+      homeBtn[1].style.opacity = "1"
+    }
+  })
+})
